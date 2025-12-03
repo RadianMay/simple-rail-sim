02eeaebfb0b55ac6a927eed9d4eceb04004fd65e
RadianMay <anastasia.may.wong@proton.me>
2025-12-02 21:35:53 -0500

    Add new scenarios for Metra Electric and UP-N lines with 160kph speed limits

---
+++ scenarios/metra_metric/metra_electric/ME_main_express_160kph.py
@@ -0,0 +1,74 @@
+scenario_name = "Metra Electric - Main Line Through Running, local (160km/h)"
+
+# This scenario sets the max speed to 160kph and uses mile markers in km. The start is set to McCormick Place.
+
+
+# Each tuple: (start milepost, end milepost, speed limit (km/h), ends at stop, name)
+
+# Mileposts are from https://chicagorailfan.com/mmelec.html
+# Assume 100mph/160kph speed limit
+
+route = [
+                                # Union stop
+    (0.0, 1.9, 80, True),
+                                # Roosevelt stop
+    (1.9, 4.3, 80, True),
+                                # McCormick Place/23rd Street stop
+    (4.3, 11.4, 160, True),
+                                # 55-56-57th Street stop
+    (11.4, 13.8, 160, True),                            
+                                # 63rd Street stop
+    (13.8, 20.5, 160, True),
+                                # 95th-Chicago State University stop
+    (20.5, 24.3, 160, True),
+                                # 115th-Kensington stop
+    (24.3, 33.3, 160, True),
+                                # Harvey stop 
+    (33.3, 39.2, 160, True),
+                                # Homewood stop 
+    (39.0, 41.7, 160, True),
+                                # Floorsmoor stop 
+    (41.7, 44.2, 160, True),
+                                # Olympia fields stop 
+    (44.2, 46.0, 160, True),
+                                # 211st Street stop 
+    (46.0, 48.3, 160, True),
+                                # Richton Park stop 
+    (48.3, 51.5, 160, True),
+                                # University Park stop
+    (51.5, 56.3, 160, True),
+                                # Monee stop 
+    (56.3, 66.7, 160, True),
+                                # Peotone stop 
+    (66.7, 76.5, 160, True),
+                                # Manteno stop 
+    (76.5, 88.8, 160, True),
+                                # Bradley stop 
+    (88.8, 91.4, 160, True),
+                                # Kankakee stop 
+]
+
+stops = {
+0.0: 'Union',
+1.9: 'Roosevelt',
+4.3: 'McCormick Place/23rd Street',
+11.4: '55-56-57th Street',
+13.8: '63rd Street',
+20.5: '95th-Chicago State University',
+24.3: '115th-Kensington',
+33.3: 'Harvey',
+39.2: 'Homewood',
+41.7: 'Floorsmoor',
+44.2: 'Olympia Fields',
+46.0: '211st Street',
+48.3: 'Richton Park',
+51.5: 'University Park',
+56.3: 'Monee',
+66.7: 'Peotone',
+76.5: 'Manteno',
+88.8: 'Bradley',
+91.4: 'Kankakee'
+}
+
+dwell_time = 30 # seconds
+pad_factor = 1.07 # 7% padding for acceleration/cruise/deceleration
--- scenarios/metra_metric/metra_electric/ME_main_local_160kph.py
+++ scenarios/metra_metric/metra_electric/ME_main_local_160kph.py
@@ -9,83 +9,89 @@ scenario_name = "Metra Electric - Main Line Through Running, local (160km/h)"
 # Assume 100mph/160kph speed limit
 
 route = [
+                                # Union stop
+    (0.0, 1.9, 80, True),
+                                # Roosevelt stop
+    (1.9, 4.3, 80, True),
                                 # McCormick Place/23rd Street stop
-    (0.0, 1.5, 160, True),
+    (4.3, 5.8, 160, True),
                                 # 31st Street stop
-    (1.5, 3.3, 160, True),
+    (5.8, 7.6, 160, True),
                                 # 39th Street-Oakland stop
-    (3.3, 5.2, 160, True),
+    (7.6, 9.5, 160, True),
                                 # 47th Street stop
-    (5.2, 6.1, 160, True),
+    (9.5, 10.4, 160, True),
                                 # 51-53rd Street-Hyde Park stop
-    (6.1, 7.1, 160, True),
+    (10.4, 11.4, 160, True),
                                 # 55-56-57th Street stop
-    (7.1, 8.7, 160, True),                            
+    (11.4, 13.0, 160, True),                            
                                 # 59-60th St-U Chicago top
-    (8.7, 9.5, 160, True),
+    (13.0, 13.8, 160, True),
                                 # 63rd Street stop
-    (9.5, 11.7, 160, True),
+    (13.8, 16.0, 160, True),
                                 # 75th-Grand Crossing stop
-    (11.7, 12.8, 160, True),
+    (16.0, 17.1, 160, True),
                                 # 79th-Chatham stop
-    (12.8, 13.3, 160, True),
+    (17.1, 17.6, 160, True),
                                 # 83rd-Avalon Park stop
-    (13.3, 14.1, 160, True),
+    (17.6, 18.4, 160, True),
                                 # 87th-Woodruff stop
-    (14.1, 14.9, 160, True),
-                                # 91st-Chesterfield stop
-    (14.9, 16.2, 160, True),
-                                # 95th-Chicago State University stop
-    (16.2, 17.8, 160, True),
-                                # 103rd Street stop
-    (17.8, 18.4, 160, True),
-                                # 107th Street stop
     (18.4, 19.2, 160, True),
+                                # 91st-Chesterfield stop
+    (19.2, 20.5, 160, True),
+                                # 95th-Chicago State University stop
+    (20.5, 22.1, 160, True),
+                                # 103rd Street stop
+    (22.1, 22.7, 160, True),
+                                # 107th Street stop
+    (22.7, 23.5, 160, True),
                                 # 111th-Pullman stop
-    (19.2, 20.0, 160, True),
+    (23.5, 24.3, 160, True),
                                 # 115th-Kensington PARK stop
-    (20.0, 24.8, 160, True),
+    (24.3, 29.1, 160, True),
                                 # Riverdale stop 
-    (24.9, 26.5, 160, True),
+    (29.2, 30.8, 160, True),
                                 # Ivanhoe stop 
-    (26.5, 27.4, 160, True),
+    (30.8, 31.7, 160, True),
                                 # 147th Street stop 
-    (27.4, 29.0, 160, True),
+    (31.7, 33.3, 160, True),
                                 # Harvey stop 
-    (29.0, 32.7, 160, True),
+    (33.3, 37.0, 160, True),
                                 # Hazel Crest stop 
-    (32.7, 33.7, 160, True),
+    (37.0, 38.0, 160, True),
                                 # Calumet stop 
-    (33.7, 34.9, 160, True),
+    (38.0, 39.2, 160, True),
                                 # Homewood stop 
 ]
 
 stops = {
-0.00: 'McCormick Place/23rd Street',
-1.5: '31st Street',
-3.3: '39th Street-Oakland',
-5.2: '47th Street',
-6.1: '51-53rd Street-Hyde Park',
-7.1: '55-56-57th Street',
-8.7: '59-60th St-U Chicago',
-9.5: '63rd Street',
-11.7: '75th-Grand Crossing',
-12.8: '79th-Chatham',
-13.3: '83rd-Avalon Park',
-14.1: '87th-Woodruff',
-14.9: '91st-Chesterfield',
-16.2: '95th-Chicago State University',
-17.8: '103rd Street',
-18.4: '107th Street',
-19.2: '111th-Pullman',
-20.0: '115th-Kensington',
-24.8: 'Riverdale',
-26.5: 'Ivanhoe',
-27.4: '147th Street',
-29.0: 'Harvey',
-32.7: 'Hazel Crest',
-33.7: 'Calumet',
-34.9: 'Homewood',
+0.0: 'Union',
+1.9: 'Roosevelt',
+4.30: 'McCormick Place/23rd Street',
+5.8: '31st Street',
+7.6: '39th Street-Oakland',
+9.5: '47th Street',
+10.4: '51-53rd Street-Hyde Park',
+11.4: '55-56-57th Street',
+13.0: '59-60th St-U Chicago',
+13.8: '63rd Street',
+16.0: '75th-Grand Crossing',
+17.1: '79th-Chatham',
+17.6: '83rd-Avalon Park',
+18.4: '87th-Woodruff',
+19.2: '91st-Chesterfield',
+20.5: '95th-Chicago State University',
+22.1: '103rd Street',
+22.7: '107th Street',
+23.5: '111th-Pullman',
+24.3: '115th-Kensington',
+29.1: 'Riverdale',
+30.8: 'Ivanhoe',
+31.7: '147th Street',
+33.3: 'Harvey',
+37.0: 'Hazel Crest',
+38.0: 'Calumet',
+39.2: 'Homewood',
 }
 
 dwell_time = 30 # seconds
